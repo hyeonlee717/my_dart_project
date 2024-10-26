24e5ee3f14bf0ba42c92d89f6c6b7e9477deb257
Hyeon Lee <103764100+hyeonlee717@users.noreply.github.com>
2024-10-26 19:10:50 +0900

    null safety

--- bin/my_dart_project.dart
+++ bin/my_dart_project.dart
@@ -1,5 +1,4 @@
-import 'package:my_dart_project/my_dart_project.dart' as my_dart_project;
-
-void main(List<String> arguments) {
-  print('Hello world: ${my_dart_project.calculate()}!');
+void main() {
+String? nico = 'nico';
+nico = null;
 }
