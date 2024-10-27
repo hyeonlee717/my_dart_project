2680d5e495c5f283c344a74afcddd64c93ec5a1e
Hyeon Lee <103764100+hyeonlee717@users.noreply.github.com>
2024-10-27 21:12:36 +0900

    list

--- bin/my_dart_project.dart
+++ bin/my_dart_project.dart
@@ -1,4 +1,18 @@
 void main() {
-String? nico = 'nico';
-nico = null;
+
+var oldFriends = ['찬우', '동현'];
+
+var newFriends = ['승현',
+
+'민우',
+
+'선용',
+
+for (var friend in oldFriends) '🥰 $friend', // Collection for 사용
+
+];
+
+
+print(newFriends); //[승현, 민우, 선용, 🥰 찬우, 🥰 동현]이 출력됨
+
 }
