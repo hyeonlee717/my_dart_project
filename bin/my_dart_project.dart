f815a2e32d7755ebb5876da13eb32e1eabbc153c
Hyeon Lee <103764100+hyeonlee717@users.noreply.github.com>
2024-10-28 20:35:31 +0900

    named constructor parameters

--- bin/my_dart_project.dart
+++ bin/my_dart_project.dart
@@ -1,17 +1,34 @@
 class Player {
   final String name;
   int xp;
+  String team;
+  int age;
 
-  Player(this.name, this.xp);
+  Player({
+    required this.name, 
+    required this.xp, 
+    required this.team, 
+    required this.age,
+    });
 
   void sayHello() {
-    print("Hi my name is $name");
+    print("Hi my name is $name my xp is $xp my team is $team my age is $age");
   }
 }
 
 void main() {
-  var player = Player("nico", 1500);
+  var player = Player(
+    name: "nico",
+    xp: 1500,
+    team: "red",
+    age: 12,
+    );
   player.sayHello();
-  var player2 = Player("lynn", 2500);
+  var player2 = Player(
+    name: "lynn",
+    xp: 2500,
+    team: "blue",
+    age: 15,
+    );
   player2.sayHello();
 }
