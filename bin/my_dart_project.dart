c3015854b835503eee928b6dee64c1c12e5601ae
Hyeon Lee <103764100+hyeonlee717@users.noreply.github.com>
2024-10-28 11:12:57 +0900

    qq

--- bin/my_dart_project.dart
+++ bin/my_dart_project.dart
@@ -1,18 +1,6 @@
+String capitalizeName(String? name) => name?.toUpperCase() ?? 'ANON';
+
 void main() {
-
-var oldFriends = ['찬우', '동현'];
-
-var newFriends = ['승현',
-
-'민우',
-
-'선용',
-
-for (var friend in oldFriends) '🥰 $friend', // Collection for 사용
-
-];
-
-
-print(newFriends); //[승현, 민우, 선용, 🥰 찬우, 🥰 동현]이 출력됨
-
+capitalizeName('nico');
+capitalizeName(null);
 }
