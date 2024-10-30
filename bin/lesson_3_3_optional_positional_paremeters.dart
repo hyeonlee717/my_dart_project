c35fb18b2e1fb4554c50968a595fffb949c33891
Hyeon Lee <103764100+hyeonlee717@users.noreply.github.com>
2024-10-30 16:21:20 +0900

    .

--- bin/lesson_3_3_optional_positional_paremeters.dart
+++ bin/lesson_3_3_optional_positional_paremeters.dart
@@ -1,10 +1,12 @@
-String sayHello(String name, int age,
-        [String? country =
-            'cuba'] // country만 not required하게 하려면 이렇게 하면 됨, 근데 ?(null safety)와 ='cuba'(초기값) 둘 중에 하나만 해도 무방함
+String sayHello(
+        {required String name,
+        required int age,
+        String? country =
+            'cuba'} // country만 not required하게 하려면 이렇게 하면 됨, 근데 ?(null safety)와 ='cuba'(초기값) 둘 중에 하나만 해도 무방함
         ) =>
     'Hello I\'m $name, you are $age years old from $country';
 
 void main() {
-  var result = sayHello('nico', 12);
+  var result = sayHello(name: 'nico', age: 12);
   print(result);
 }
